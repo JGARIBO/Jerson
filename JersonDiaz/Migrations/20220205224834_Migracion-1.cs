14fb5da235b739060feaa310b2a7f8e3ec273a07
JGARIBO <thesoftware.garibodiaz@gmail.com>
2022-02-05 17:03:02 -0600

    subir

---
+++ JersonDiaz/Migrations/20220205224834_Migracion-1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JersonDiaz.Migrations
+{
+    public partial class Migracion1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(maxLength: 200, nullable: false),
+                    Apellido = table.Column<string>(maxLength: 200, nullable: false),
+                    Telefono = table.Column<string>(maxLength: 20, nullable: false),
+                    Direccion = table.Column<string>(maxLength: 500, nullable: false),
+                    Cedula = table.Column<string>(maxLength: 50, nullable: false),
+                    Genero = table.Column<string>(maxLength: 100, nullable: false),
+                    IdUsuario = table.Column<int>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
